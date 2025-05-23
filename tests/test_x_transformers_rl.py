ff80f4dac246b1b53d2cd1c0aaaf90626870d4ca
lucidrains <lucidrains@gmail.com>
2025-05-23 11:03:10 -0700

    e2e in test

--- tests/test_x_transformers_rl.py
+++ tests/test_x_transformers_rl.py
@@ -1,4 +1,40 @@
 import pytest
+import numpy as np
 
 def test_e2e():
-    assert True
+    class Sim:
+        def reset(self, seed = None):
+            return np.random.randn(5) # state
+
+        def step(self, actions):
+            return np.random.randn(5), np.random.randn(1), False # state, reward, done
+
+    sim = Sim()
+
+    # learning
+
+    from x_transformers_rl import Learner
+
+    learner = Learner(
+        state_dim = 5,
+        num_actions = 2,
+        reward_range = (-1., 1.),
+        max_timesteps = 10,
+        batch_size = 2,
+        update_episodes = 2,
+        world_model = dict(
+            attn_dim_head = 16,
+            heads = 4,
+            depth = 1,
+        )
+    )
+
+    learner(sim, 1)
+
+    # deploying
+
+    agent = learner.agent
+
+    hiddens = None
+    actions, hiddens = agent(np.random.randn(6), hiddens = hiddens)
+    actions, hiddens = agent(np.random.randn(6), hiddens = hiddens)
